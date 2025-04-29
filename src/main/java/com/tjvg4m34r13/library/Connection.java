0cb6747b34cc08f896ab6fd43fa0a4b72532be79
thomasjeferssonvaz <thomasjeferssonvaz@alunos.utfpr.edu.br>
2025-04-29 20:03:21 -0300

    Start Commit

--- src/main/java/com/tjvg4m34r13/library/Connection.java
+++ src/main/java/com/tjvg4m34r13/library/Connection.java
@@ -3,63 +3,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.tjvg4m34r13.library;
-import java.sql.*;
+import okhttp3.Call;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
 
 /**
  * @author thomas.vaz
  */
 public class Connection {
-    public static void main(String[] args) {
-        String url = "jdbc:mysql://mysql.thomasjeferssonvaz.dev.br:3306"; //pointing to no database.
-        String username = "library";
-        String password = "$Jefersson3435";
-
-        System.out.println("Connecting to server...");
-
-        try (java.sql.Connection connection = DriverManager.getConnection(url, username, password)) {
-            System.out.println("Server connected!");
-            Statement stmt = null;
-            ResultSet resultset = null;
-
-            try {
-                stmt = connection.createStatement();
-                resultset = stmt.executeQuery("SHOW DATABASES;");
-
-                if (stmt.execute("SHOW DATABASES;")) {
-                    resultset = stmt.getResultSet();
-                }
-
-                while (resultset.next()) {
-                    System.out.println(resultset.getString("Database"));
-                }
-            } catch (SQLException ex) {
-                // handle any errors
-                ex.printStackTrace();
-            } finally {
-                // release resources
-                if (resultset != null) {
-                    try {
-                        resultset.close();
-                    } catch (SQLException sqlEx) {
-                    }
-                    resultset = null;
-                }
-
-                if (stmt != null) {
-                    try {
-                        stmt.close();
-                    } catch (SQLException sqlEx) {
-                    }
-                    stmt = null;
-                }
-
-                if (connection != null) {
-                    connection.close();
-                }
-            }
-        } catch (SQLException e) {
-            throw new IllegalStateException("Cannot connect the server!", e);
-        }
+    public static void main(String[] args) throws IOException {
+        Request request = new Request()
+                .url(BASE_URL + "/date")
+                .build();
+        Call call = client.newCall(request);
+        Response response = call.execute();
     }
-
 }
